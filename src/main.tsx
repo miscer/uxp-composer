7cb5cc8dd8ed8700847ad8781fc29d9e50242b62
Michal Miškerník <michal@miskernik.sk>
2024-07-02 21:26:12 +0200

    Listing parameter values

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,98 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import "./main.css";
 
 const ppro = require("premierepro");
 
 export const App = () => {
+  const [editableClips, setEditableClips] = useState<EditableClip[]>([]);
+
+  function loadData() {
+    getEditableClips().then(setEditableClips).catch(err => console.error(err));
+  }
+
   return (
     <div className="app">
       <h1>Animation Composer</h1>
       <button onClick={loadData}>Load Data</button>
+
+      {editableClips.map(editableClip => (
+        <div key={editableClip.name}>
+          <h2>{editableClip.name}</h2>
+          {editableClip.params.map(param => (
+            <div key={param.name}>
+              {param.name}: {JSON.stringify(param.keyframe.value)}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
 
-async function loadData() {
+interface Param {
+  name: string;
+  keyframe: any;
+}
+
+interface EditableClip {
+  name: string;
+  params: Param[];
+  trackItem: any;
+  component: any;
+}
+
+async function getEditableClips() {
   const project = await ppro.Project.getActiveProject();
   if (!project) {
     console.log("No active project found");
-    return;
+    return [];
   }
 
   const sequence = await project.getActiveSequence();
   if (!sequence) {
     console.log("No active sequence found");
-    return;
+    return [];
   }
-  console.log("sequence", sequence);
 
-  const videoTrack = await sequence.getVideoTrack(0);
-  console.log("video track", videoTrack);
+  const result: EditableClip[] = [];
+
+  const numVideoTracks = await sequence.getVideoTrackCount();
+  for (let i = 0; i < numVideoTracks; i++) {
+    const videoTrack = await sequence.getVideoTrack(i);
+    const clips = await videoTrack.getTrackItems(ppro.Constants.TrackItemType.CLIP, false);
+
+    for (const clip of clips) {
+      const projectItem = await clip.getProjectItem();
+      const components = await clip.getComponentChain();
+      const numComponents = components.getComponentCount();
+
+      for (let j = 0; j < numComponents; j++) {
+        const component = components.getComponentAtIndex(j);
+        if (await component.getMatchName() !== "AE.ADBE Capsule") continue;
+
+        const editableClip: EditableClip = {
+          name: projectItem.name,
+          trackItem: clip,
+          component: component,
+          params: [],
+        };
+
+        const numParams = await component.getParamCount();
+        for (let k = 0; k < numParams; k++) {
+          const param = await component.getParam(k);
+
+          editableClip.params.push({
+            name: param.displayName,
+            keyframe: await param.getStartValue(),
+          });
+        }
+
+        result.push(editableClip);
+      }
+    }
+  }
+
+  console.log("result", result);
+
+  return result;
 }
